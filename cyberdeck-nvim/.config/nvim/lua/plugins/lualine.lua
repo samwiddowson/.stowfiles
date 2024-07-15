9e583f9d58222d2eaa4d6fd57d8ced26446a41ef
Sam Widdowson <samwid@gmail.com>
2024-07-15 12:00:49 +0100

    cyberdeck tmux theme changes

--- cyberdeck-nvim/.config/nvim/lua/plugins/lualine.lua
+++ cyberdeck-nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,7 @@ return
         lualine.setup {
             options = {
                 icons_enabled = true,
-                theme = 'solarized_dark',
+                theme = 'everforest',
                 section_separators = { left = '', right = '' },
                 component_separators = { left = '', right = '' },
                 disabled_filetypes = {}
