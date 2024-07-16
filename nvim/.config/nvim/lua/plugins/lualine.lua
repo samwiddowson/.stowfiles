dde4486e4b2d4875b77609ec32e66afd4f44c2b6
Sam Widdowson <samwid@gmail.com>
2024-07-16 23:42:40 +0100

    Changes to desktop theming

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,9 +9,9 @@ return
         lualine.setup {
             options = {
                 icons_enabled = true,
-                theme = 'solarized_dark',
-                section_separators = { left = '', right = '' },
-                component_separators = { left = '', right = '' },
+                theme = 'everforest',
+                section_separators = { left = '', right = '' },
+                component_separators = { left = '', right = '' },
                 disabled_filetypes = {}
             },
             sections = {
