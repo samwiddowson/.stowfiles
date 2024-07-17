44fd875c1923a3f4d456db32fca32d9569f29caf
Sam Widdowson <samwid@gmail.com>
2024-07-17 01:47:49 +0100

    Fix obsidian config

--- cyberdeck-nvim/.config/nvim/lua/plugins/obsidian.lua
+++ cyberdeck-nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -3,8 +3,8 @@ return {
     version = "*",  -- recommended, use latest release instead of latest commit
     lazy = true,
     ft = "markdown",
-                preferred_link_style = "wiki",
-                new_notes_location = "notes_subdir",
+    preferred_link_style = "wiki",
+    new_notes_location = "notes_subdir",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
     -- event = {
         --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
@@ -37,115 +37,143 @@ return {
                 -- Set to false to disable completion.
                 nvim_cmp = true,
                 -- Trigger completion at 2 chars.
-                min_chars = 2,
+                min_chars = 2
+            },
+            mappings = {
+                -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+                ["gf"] = {
+                    action = function()
+                        return require("obsidian").util.gf_passthrough()
+                    end,
+                    opts = { noremap = false, expr = true, buffer = true },
+                },
+                -- Toggle check-boxes.
+                ["<leader>ch"] = {
+                    action = function()
+                        return require("obsidian").util.toggle_checkbox()
+                    end,
+                    opts = { buffer = true },
+                },
+                -- Smart action depending on context, either follow link or toggle checkbox.
+                ["<cr>"] = {
+                    action = function()
+                        return require("obsidian").util.smart_action()
+                    end,
+                    opts = { buffer = true, expr = true },
+                }
+            },
+            -- Where to put new notes. Valid options are
+            --  * "current_dir" - put new notes in same directory as the current buffer.
+            --  * "notes_subdir" - put new notes in the default notes subdirectory.
+            wiki_link_func = "use_alias_only",
+            -- Either 'wiki' or 'markdown'.
+
+            -- Optional, for templates (see below).
+            templates = {
+                folder = "5 - Templates",
+                date_format = "%Y-%m-%d",
+                time_format = "%H:%M",
+                -- A map for custom variables, the key should be the variable and the value a function
+                substitutions = {},
+            },   -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
+
+            picker = {
+                -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+                name = "telescope.nvim",
+                -- Optional, configure key mappings for the picker. These are the defaults.
+                -- Not all pickers support all mappings.
                 mappings = {
-                    -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-                    ["gf"] = {
-                        action = function()
-                            return require("obsidian").util.gf_passthrough()
-                        end,
-                        opts = { noremap = false, expr = true, buffer = true },
-                    },
-                    -- Toggle check-boxes.
-                    ["<leader>ch"] = {
-                        action = function()
-                            return require("obsidian").util.toggle_checkbox()
-                        end,
-                        opts = { buffer = true },
-                    },
-                    -- Smart action depending on context, either follow link or toggle checkbox.
-                    ["<cr>"] = {
-                        action = function()
-                            return require("obsidian").util.smart_action()
-                        end,
-                        opts = { buffer = true, expr = true },
-                    }
+                    -- Create a new note from your query.
+                    new = "<C-x>",
+                    -- Insert a link to the selected note.
+                    insert_link = "<C-l>",
                 },
-                -- Where to put new notes. Valid options are
-                --  * "current_dir" - put new notes in same directory as the current buffer.
-                --  * "notes_subdir" - put new notes in the default notes subdirectory.
-                wiki_link_func = "use_alias_only",
-                -- Either 'wiki' or 'markdown'.
+            },
+            -- Optional, sort search results by "path", "modified", "accessed", or "created".
+            -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
+            -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
+            sort_by = "modified",
+            sort_reversed = true,
 
-                -- Optional, for templates (see below).
-                templates = {
-                    folder = "5- Templates",
-                    date_format = "%Y-%m-%d",
-                    time_format = "%H:%M",
-                    -- A map for custom variables, the key should be the variable and the value a function
-                    substitutions = {},
-                },   -- Optional, set to true to force ':ObsidianOpen' to bring the app to the foreground.
+            -- Set the maximum number of lines to read from notes on disk when performing certain searches.
+            search_max_lines = 1000,
 
-                picker = {
-                    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-                    name = "telescope.nvim",
-                    -- Optional, configure key mappings for the picker. These are the defaults.
-                    -- Not all pickers support all mappings.
-                    mappings = {
-                        -- Create a new note from your query.
-                        new = "<C-x>",
-                        -- Insert a link to the selected note.
-                        insert_link = "<C-l>",
-                    },
-                },
-                -- Optional, sort search results by "path", "modified", "accessed", or "created".
-                -- The recommend value is "modified" and `true` for `sort_reversed`, which means, for example,
-                -- that `:ObsidianQuickSwitch` will show the notes sorted by latest modified time
-                sort_by = "modified",
-                sort_reversed = true,
+            -- Optional, determines how certain commands open notes. The valid options are:
+            -- 1. "current" (the default) - to always open in the current window
+            -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
+            -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
+            open_notes_in = "current",
+            disable_frontmatter = false,
+  -- Optional, customize how note IDs are generated given an optional title.
+  ---@param title string|?
+  ---@return string
+  note_id_func = function(title)
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return tostring(os.time()) .. "-" .. suffix
+  end,
+            -- Optional, customize how note file names are generated given the ID, target directory, and title.
+            ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+            ---@return string|obsidian.Path The full path to the new note.
+            note_path_func = function(spec)
+                -- This is equivalent to the default behavior.
+                local path = spec.dir / tostring(spec.title)
+                return path:with_suffix(".md")
+            end,
 
-                -- Set the maximum number of lines to read from notes on disk when performing certain searches.
-                search_max_lines = 1000,
-
-                -- Optional, determines how certain commands open notes. The valid options are:
-                -- 1. "current" (the default) - to always open in the current window
-                -- 2. "vsplit" - to open in a vertical split if there's not already a vertical split
-                -- 3. "hsplit" - to open in a horizontal split if there's not already a horizontal split
-                open_notes_in = "current",
-
-                -- Optional, configure additional syntax highlighting / extmarks.
-                -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
-                ui = {
-                    enable = true,  -- set to false to disable all additional syntax features
-                    update_debounce = 200,  -- update delay after a text change (in milliseconds)
-                    max_file_length = 5000,  -- disable UI features for files with more than this many lines
-                    -- Define how various check-boxes are displayed
-                    checkboxes = {
-                        -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-                        [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-                        ["x"] = { char = "", hl_group = "ObsidianDone" },
-                        [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-                        ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
-                        ["!"] = { char = "", hl_group = "ObsidianImportant" },
-                        -- Replace the above with this if you don't have a patched font:
-                        -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-                        -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-
-                        -- You can also add more custom ones...
-                    },
-                    -- Use bullet marks for non-checkbox lists.
-                    bullets = { char = "•", hl_group = "ObsidianBullet" },
-                    external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+            -- Optional, configure additional syntax highlighting / extmarks.
+            -- This requires you have `conceallevel` set to 1 or 2. See `:help conceallevel` for more details.
+            ui = {
+                enable = true,  -- set to false to disable all additional syntax features
+                update_debounce = 200,  -- update delay after a text change (in milliseconds)
+                max_file_length = 5000,  -- disable UI features for files with more than this many lines
+                -- Define how various check-boxes are displayed
+                checkboxes = {
+                    -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+                    [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+                    ["x"] = { char = "", hl_group = "ObsidianDone" },
+                    [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+                    ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+                    ["!"] = { char = "", hl_group = "ObsidianImportant" },
                     -- Replace the above with this if you don't have a patched font:
-                    -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-                    reference_text = { hl_group = "ObsidianRefText" },
-                    highlight_text = { hl_group = "ObsidianHighlightText" },
-                    tags = { hl_group = "ObsidianTag" },
-                    block_ids = { hl_group = "ObsidianBlockID" },
-                    hl_groups = {
-                        -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-                        ObsidianTodo = { bold = true, fg = "#f78c6c" },
-                        ObsidianDone = { bold = true, fg = "#89ddff" },
-                        ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
-                        ObsidianTilde = { bold = true, fg = "#ff5370" },
-                        ObsidianImportant = { bold = true, fg = "#d73128" },
-                        ObsidianBullet = { bold = true, fg = "#89ddff" },
-                        ObsidianRefText = { underline = true, fg = "#c792ea" },
-                        ObsidianExtLinkIcon = { fg = "#c792ea" },
-                        ObsidianTag = { italic = true, fg = "#89ddff" },
-                        ObsidianBlockID = { italic = true, fg = "#89ddff" },
-                        ObsidianHighlightText = { bg = "#75662e" },
-                    },
+                    -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+                    -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+                    -- You can also add more custom ones...
+                },
+                -- Use bullet marks for non-checkbox lists.
+                bullets = { char = "•", hl_group = "ObsidianBullet" },
+                external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+                -- Replace the above with this if you don't have a patched font:
+                -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
+                reference_text = { hl_group = "ObsidianRefText" },
+                highlight_text = { hl_group = "ObsidianHighlightText" },
+                tags = { hl_group = "ObsidianTag" },
+                block_ids = { hl_group = "ObsidianBlockID" },
+                hl_groups = {
+                    -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
+                    ObsidianTodo = { bold = true, fg = "#f78c6c" },
+                    ObsidianDone = { bold = true, fg = "#89ddff" },
+                    ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
+                    ObsidianTilde = { bold = true, fg = "#ff5370" },
+                    ObsidianImportant = { bold = true, fg = "#d73128" },
+                    ObsidianBullet = { bold = true, fg = "#89ddff" },
+                    ObsidianRefText = { underline = true, fg = "#c792ea" },
+                    ObsidianExtLinkIcon = { fg = "#c792ea" },
+                    ObsidianTag = { italic = true, fg = "#89ddff" },
+                    ObsidianBlockID = { italic = true, fg = "#89ddff" },
+                    ObsidianHighlightText = { bg = "#75662e" },
                 },
             },
         },
