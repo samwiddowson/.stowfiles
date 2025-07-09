bf93b93d92a6dc4e14c261fb3cc13dc87b1e932e
Sam Widdowson <sam.widdowson1@nhs.net>
2025-07-09 11:22:40 +0100

    harpoon keybinds changed to follow keyboard remap

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -8,9 +8,9 @@ return {
         vim.keymap.set("n", "<leader>a", mark.add_file)
         vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-        vim.keymap.set("n", "<M-7>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<M-8>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<M-9>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<M-0>", function() ui.nav_file(4) end)
+        vim.keymap.set("n", "<M-)>", function() ui.nav_file(1) end)
+        vim.keymap.set("n", "<M-}>", function() ui.nav_file(2) end)
+        vim.keymap.set("n", "<M-]>", function() ui.nav_file(3) end)
+        vim.keymap.set("n", "<M->>", function() ui.nav_file(4) end)
     end
 }
