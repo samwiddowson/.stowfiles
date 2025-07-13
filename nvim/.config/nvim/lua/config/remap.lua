319bf32f9d96dd1a96a21cba7b25a4c4d261afa5
Sam Widdowson <samwid@gmail.com>
2025-07-13 23:50:19 +0100

    Add twilight and render markdown plugins. Add descriptions for key mappings

--- nvim/.config/nvim/lua/config/remap.lua
+++ nvim/.config/nvim/lua/config/remap.lua
@@ -2,7 +2,7 @@ vim.g.mapleader = " "
 
 local map = vim.keymap.set
 
-map("n", "<leader>pv", vim.cmd.NvimTreeOpen)
+map("n", "<leader>pv", vim.cmd.NvimTreeOpen, { desc = "Open NvimTree" })
 
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
@@ -13,17 +13,17 @@ map("n", "<C-u>", "<C-u>zz")
 map("n", "n", "nzzzv")
 map("n", "N", "Nzzzv")
 
-map("x", "<leader>p", "\"_dP")
+map("x", "<leader>p", "\"_dP", { desc = "Put (keep register)" })
 
-map("n", "<leader>y", "\"+y")
-map("v", "<leader>y", "\"+y")
+map("n", "<leader>y", "\"+y", { desc = "yank to clipboard" })
+map("v", "<leader>y", "\"+y", { desc = "yank to clipboard" })
 map("n", "<leader>Y", "\"+Y")
 
-map("n", "<leader><leader>pp", "\"+p")
+map("n", "<leader><leader>pp", "\"+p", { desc = "Put from clipboard" })
 
 map("v", "<C-c>", "\"+yi")
 map("v", "<C-x>", "\"+c")
-map("v", "<C-v>", "c<Esc>\"+p")
+-- map("v", "<C-v>", "c<Esc>\"+p")
 map("i", "<C-v>", "<Esc>\"+pa")
 
 map("n", "Q", "<nop>")
@@ -38,15 +38,15 @@ map("n", "q:", "<nop>")
 -- map("n", "<leader>k", "<cmd>lnext<CR>zz")
 -- map("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word in file" })
 -- map("v", "<leader>s", "\"+y:%s/<C-V>/<C-V>/gI<Left><Left><Left>")
 map("v", "<leader>s", function()
     vim.cmd('normal! "+y')
     local selected = vim.fn.getreg("+")
     local cmd = ':%s/' .. selected .. '/' .. selected .. '/gI<Left><Left><Left>'
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(cmd, true, false, true), 'n', false)
-end)
+end, { desc = "Replace selection in file" })
 
 map("n", "<leader>nn", function() --toggle relative vs absolute line numbers
     vim.wo.relativenumber = not vim.wo.relativenumber
-end)
+end, { desc = "toggle relative line numbers" })
--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -5,11 +5,11 @@ return {
         local fzf = require('fzf-lua')
         fzf.setup()
         vim.keymap.set('n', '<C-p>', fzf.git_files, {})
-        vim.keymap.set('n', '<leader>po', fzf.files, {})
-        vim.keymap.set('n', '<leader>r', fzf.registers, {})
-        vim.keymap.set('n', '<leader>m', fzf.marks, {})
-        vim.keymap.set('n', '<leader>b', fzf.buffers, {})
-        vim.keymap.set('n', '<leader>i', fzf.live_grep, {})
-        vim.keymap.set('n', '<leader>k', fzf.keymaps, {})
+        vim.keymap.set('n', '<leader>po', fzf.files, { desc = "Open file in directory" })
+        vim.keymap.set('n', '<leader>r', fzf.registers, { desc = "View registers" })
+        vim.keymap.set('n', '<leader>m', fzf.marks, { desc = "View marks" })
+        vim.keymap.set('n', '<leader>b', fzf.buffers, { desc = "View open files" })
+        vim.keymap.set('n', '<leader>i', fzf.live_grep, { desc = "Live grep" })
+        vim.keymap.set('n', '<leader>k', fzf.keymaps, { desc = "View keymaps" })
     end
 }
