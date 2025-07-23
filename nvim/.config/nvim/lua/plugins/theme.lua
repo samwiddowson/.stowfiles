069541fd5559537a12f05658b19ba83b199e0a4e
Sam Widdowson <samwid@gmail.com>
2025-07-23 23:56:06 +0100

    Clean up nvim theme plugins

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,20 +1,8 @@
 return {
-    "EdenEast/nightfox.nvim",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
     dependencies = {
-        { "blazkowolf/gruber-darker.nvim" },
         { "neanias/everforest-nvim" },
-        { "catppuccin/nvim" },
-        { "xero/miasma.nvim" },
-        { 'sainnhe/gruvbox-material' },
     },
-    config = function()
-        require("everforest").setup({
-            colours_override = function(palette)
-                -- palette.green = "#dbbc7f"
-                -- palette.yellow = "#a7c080"
-            end
-        })
-    end
 }
