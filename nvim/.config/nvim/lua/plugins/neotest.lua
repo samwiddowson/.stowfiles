fade8433da258fc5b6ae5557a230ff168fc870eb
Sam Widdowson <sam.widdowson1@nhs.net>
2025-09-26 15:49:45 +0100

    Add neotest

---
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,38 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "rouge8/neotest-rust",
+    },
+    config = function()
+        local neotest = require("neotest")
+        neotest.setup({
+            adapters = {
+                require("neotest-rust")
+            }
+        })
+
+        vim.keymap.set("n", "<leader>to", function()
+            neotest.output_panel.toggle()
+        end, { desc = "NeoTest: Toggle output" })
+
+        vim.keymap.set("n", "<leader>ts", function()
+            neotest.summary.toggle()
+        end, { desc = "NeoTest: Toggle summary" })
+
+        vim.keymap.set("n", "<leader>tr", function()
+            neotest.run.run()
+        end, { desc = "NeoTest: Run current test" })
+
+        vim.keymap.set("n", "<leader>tf", function()
+            neotest.run.run(vim.fn.expand("%"))
+        end, { desc = "NeoTest: Run current file" })
+
+        vim.keymap.set("n", "<leader>tw", function()
+            neotest.watch.toggle(vim.fn.expand("%"))
+        end, { desc = "NeoTest: Toggle watch on current file" })
+    end
+}
