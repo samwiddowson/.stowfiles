36dc8569539d3908af55910263f35d389bee1933
Sam Widdowson <sam.widdowson@anaplan.com>
2025-12-15 10:35:20 +0000

    Python LSP stuff

--- nvim/.config/nvim/lsp/basedpyright.lua
+++ nvim/.config/nvim/lsp/basedpyright.lua
@@ -1,10 +1,14 @@
 return {
-    root_dir = require('lspconfig.util').root_pattern('pyrightconfig.json', '.git'),
+    root_markers = { "pyrightconfig.json", ".git" },
     settings = {
         basedpyright = {
             config = {
                 typeCheckingMode = "basic"
             }
-        }
+        },
+        -- TODO: remove when finished troubleshooting venv/dependency issue
+        -- python = {
+        --     pythonPath = "/Users/sam.widdowson/.pyenv/versions/3.9.25/envs/concord/bin/python"
+        -- }
     }
 }
