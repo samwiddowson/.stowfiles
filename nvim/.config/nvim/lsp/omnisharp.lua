78db471b5bb17b3401331f39f7ed60b9289659b7
Sam Widdowson <samwid@gmail.com>
2026-02-03 16:56:06 +0000

    Add note about omnisharp language lsp config not working

--- nvim/.config/nvim/lsp/omnisharp.lua
+++ nvim/.config/nvim/lsp/omnisharp.lua
@@ -1,4 +1,6 @@
 return {
+    -- there is some issue with this? LSP logs state
+    -- cmd: expected expected function or table with executable command, got table
     cmd = {
         "/home/sam/.local/share/nvim/mason/bin/omnisharp-mono",
         "--languageserver",
