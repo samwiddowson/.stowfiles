4e644c55dc325df7cc4ff99fbf70d7482b3acd8d
Sam Widdowson <samwid@gmail.com>
2026-02-22 19:43:38 +0000

    fixed order for power menu

--- hypr/.config/hypr/scripts/power-menu.sh
+++ hypr/.config/hypr/scripts/power-menu.sh
@@ -1,22 +1,32 @@
 #!/bin/zsh
 
-menu_items="󰌾  Lock\n󰐥  Shut Down\n󰜉  Reboot\n󰗽  Log Out"
+menu_items='󰌾  Lock\n󰐥  Shut Down\n󰗽  Log Out\n󰜉  Reboot'
+
+confirm() {
+    local action="$1"
+    local result=$(echo -e "Yes, $action\n Cancel" | wofi --dmenu \
+        --prompt "Confirm" -k /dev/null \
+        -c "$HOME/.config/wofi/power-menu.conf")
+
+    [[ "$result" == *"Yes"* ]] && return 0 || return 1
+}
+
 
 choice=$(echo -e "$menu_items" | wofi --dmenu \
-    --prompt "Power" \
+    --prompt "Power" -k /dev/null \
     -c "$HOME/.config/wofi/power-menu.conf")
 
 case "$choice" in
     *"Shut Down"*)
-        systemctl poweroff
+        confirm "shut down" && systemctl poweroff
         ;;
     *"Reboot"*)
-        systemctl reboot
+        confirm "reboot" && systemctl reboot
         ;;
     *"Lock"*)
         hyprlock
         ;;
     *"Log Out"*)
-        hyprshutdown
+        confirm "log out" && hyprshutdown
         ;;
 esac
