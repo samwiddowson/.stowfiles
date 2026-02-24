9a523951a107d573fd6ce588e84342f7fc280682
Sam Widdowson <samwid@gmail.com>
2026-02-24 23:48:24 +0000

    Tweaks: wiremix, generic focus/launch script, audio/media on waybar

---
+++ hypr/.config/hypr/scripts/focus-or-launch.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+app_name="$1"
+launch_cmd="$2"
+
+# Check if requested app is running
+if hyprctl clients -j | jq -e '.[] | select(.class == "'$app_name'")' > /dev/null 2>&1; then
+    # Focus it
+    hyprctl dispatch focuswindow "class:$app_name"
+    notify-send "Existing Terminal" "Switched to existing app $app_name on workspace $(hyprctl activeworkspace -j | jq -r '.id')."
+else
+    # Launch new kitty with tmux
+    notify-send "DEBUG" "$launch_cmd"
+    eval $launch_cmd
+fi
