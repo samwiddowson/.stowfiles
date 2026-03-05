e49bf18a1ab33d912d721329e6a830345ae9b61d
Sam Widdowson <samwid@gmail.com>
2026-03-05 21:52:06 +0000

    Add git shortcuts from macos branch

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -2,5 +2,6 @@ return {
     'tpope/vim-fugitive',
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open Vim Fugitive" })
+        vim.keymap.set("n", "<leader>ggb", ":Git blame<CR>", { desc = "Open Vim Fugitive" })
     end
 }
