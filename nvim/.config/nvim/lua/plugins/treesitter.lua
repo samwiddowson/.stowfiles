5ea5fa0b7e37695ce26900d33f4e76116e74bf07
Sam Widdowson <sam.widdowson@anaplan.com>
2026-04-16 20:00:42 +0100

    add treesitter parsers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,16 +6,35 @@ return {
     opts = {},
     init = function()
         local ensure_installed = {
-            "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query",
-            "markdown", "markdown_inline",
+            "bash", "c", "ecma", "groovy", "html", "html_tags", "java", "javascript", "json", "jsx", "kotlin", "lua",
+            "make", "markdown", "markdown_inline", "python", "query", "toml", "typescript", "vim", "vimdoc", "yaml",
+            "zsh",
         }
 
         vim.api.nvim_create_autocmd('FileType', {
-            callback = function()
+            callback = function(args)
+                local ts = require('nvim-treesitter')
+                local lang = vim.treesitter.language.get_lang(args.match)
+                if lang and vim.list_contains(ts.get_available(), lang)
+                    and not vim.list_contains(ts.get_installed(), lang) then
+                    ts.install(lang, function()
+                        vim.schedule(function()
+                            vim.treesitter.start(args.buf, lang)
+                        end)
+                    end)
+                end
                 pcall(vim.treesitter.start)
                 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
             end,
         })
+
+        vim.api.nvim_create_user_command('TSDump', function()
+            local parsers = require('nvim-treesitter').get_installed()
+            table.sort(parsers)
+            local quoted = vim.tbl_map(function(p) return '"' .. p .. '"' end, parsers)
+            local line = '            ' .. table.concat(quoted, ', ') .. ','
+            vim.api.nvim_put({ line }, 'l', true, true)
+        end, {})
 
         local installed = require('nvim-treesitter.config').get_installed()
         local to_install = vim.iter(ensure_installed)
