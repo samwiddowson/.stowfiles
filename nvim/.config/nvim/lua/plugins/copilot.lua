0db8de6b5cbcbf471a510f0e9c6e18770fd73994
Sam Widdowson <sam.widdowson@anaplan.com>
2026-04-16 09:22:25 +0100

    remove copilot plugins from neovim

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "https://github.com/github/copilot.vim",
-    config = function()
-        vim.g.copilot_no_tab_map = true
-        vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<S-Tab>")', { expr = true, replace_keycodes = false })
-    end,
-}
