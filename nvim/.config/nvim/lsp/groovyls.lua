d419b669c928f7d58b44d89b2db0456274549775
Sam Widdowson <sam.widdowson@anaplan.com>
2026-04-16 09:24:08 +0100

    Configure groovyls

---
+++ nvim/.config/nvim/lsp/groovyls.lua
@@ -0,0 +1,4 @@
+return {
+    cmd = { "java", "-jar", "/Users/sam.widdowson/local/share/nvim/mason/packages/groovy-language-server/build/libs/groovy-language-server-all.jar" },
+    filetypes = { 'groovy', 'Jenkinsfile' }
+}
