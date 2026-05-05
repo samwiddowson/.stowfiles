5bb35e97df0862124b893e090a13ec414f0b628a
Sam Widdowson <sam.widdowson@anaplan.com>
2026-05-05 15:30:40 +0100

    Replaced none-ls formatting with conform.nvim

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,19 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = {
+        formatters_by_ft = {
+            javascript      = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescript      = { "prettier" },
+            typescriptreact = { "prettier" },
+            json            = { "prettier" },
+            jsonc           = { "prettier" },
+        },
+        format_on_save = {
+            timeout_ms = 2000,
+            lsp_format = "fallback",
+        },
+    },
+}
